4df520b3b16288181d4f1322aa84c650bd00ed0f
Peter Slessing <peter@slessing.dk>
2026-03-24 15:15:02 +0100

    Dont have pose stage two times

--- src/main.cpp
+++ src/main.cpp
@@ -11,7 +11,6 @@
 #include "stages/optical_flow_stage.hpp"
 #include "stages/pose_stage.hpp"
 #include "stages/ed_ransac_stage.hpp"
-#include "stages/pose_stage.hpp"
 #include "stages/output_stage.hpp"
 
 static std::atomic<bool> g_shutdown{false};
